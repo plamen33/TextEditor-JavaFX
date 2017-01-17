a9f6189a836ac9dc4f0d1cb0965bbef056ede42f
Plamen <plam_en27@abv.bg>
2017-01-17 19:11:43 +0200

    TextEditor JavaFX version 1.0 finished. Available functionality: 1/ Load file option 2/ Save file option 3/ Close program option 4/ About option

--- TextEditorJavaFX/src/TextEditor/Controller.java
+++ TextEditorJavaFX/src/TextEditor/Controller.java
@@ -2,25 +2,49 @@ package TextEditor;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
+import javafx.scene.control.ContentDisplay;
 import javafx.scene.control.TextArea;
+import javafx.stage.FileChooser;
+
+import java.io.*;
+import java.util.Arrays;
+
 
 public class Controller {
 
     @FXML
     private TextArea textArea;
+	
+	private TextFile currentTextFile;
 
-    private Model model;
+    private Model model; 
 
     public Controller(Model model){
         this.model = model;
     }
     @FXML
     private void onSave(){
-
+       TextFile textFile = new TextFile(currentTextFile.loadFile(), Arrays.asList(textArea.getText().split("\n")));
+       model.save(textFile);
     }
     @FXML
     private void onLoad(){
+		FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Choose file to open");
+        fileChooser.setInitialDirectory(new File("./"));  // ./ - means the Directory where the program started
+        File file =  fileChooser.showOpenDialog(null);
+        if(file!=null){
+          InputOutputResult<TextFile> io =   model.load(file.toPath());
 
+            if (io.isCheck() && io.hasData()){
+                currentTextFile = io.getData();
+                textArea.clear();
+                currentTextFile.getContentFromFile().forEach(line ->textArea.appendText(line + "\n"));
+            }
+            else{
+               System.out.println("Loading failed. File cannot be rendered.");
+            }
+        }
     }
     @FXML
     private void onClose(){
